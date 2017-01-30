7a90aa59678aa280b848a1fc7056b4af0edb7fec
ggzerosum <ggzerosum@naver.com>
2017-01-30 23:24:25 +0900

    두번째
    두번째 수정 사항

---
+++ Practice_project/Practice_project/fncs.cpp
@@ -0,0 +1,4 @@
+namespace providence_010101010101_yoyoyo
+{
+	int my_score = 100;
+}
--- Practice_project/Practice_project/main.cpp
+++ Practice_project/Practice_project/main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 
+namespace providence_010101010101_yoyoyo
+{
+	extern int my_score;
+}
+
 void main(void)
 {
-
+	namespace yo = providence_010101010101_yoyoyo;
+	using std::cout;
+	cout<< yo::my_score;
 }
